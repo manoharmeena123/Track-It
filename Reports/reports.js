df43db359f9722bceec75c8eb9d9b98cf4bfb386
Ejajkhan613 <Lenovo@EJAJULANSARI.EJAJULANSARI>
2023-02-22 19:18:07 +0530

    day_1

---
+++ Reports/reports.js
@@ -0,0 +1,15 @@
+
+
+const ctx = document.getElementById('BarGraph').getContext('2d');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    }
+});
