a30703e5da93accdbdeb68b86542f37af716d36f
Ashwin-krish-nan <ashwinraj786.as@gmail.com>
2023-02-24 04:48:19 +0530

    ashday-2 added navbar and side bar in frontend

---
+++ components/sidebar1.js
@@ -0,0 +1,30 @@
+let sidebar1=()=>{
+  return`
+  <div class="shortcut">
+  <a href="timetracker.html"><img src="https://app.clockify.me/assets/nav-icons/time-tracker.svg" alt=""><span>TIME TRACKER</span></a>
+
+  <a href="calender.html"><img src="https://app.clockify.me/assets/nav-icons/calendar.svg" alt=""><span>CALENDER</span></a>
+ 
+  <a href=""><span>ANALYZE</span></a>
+ 
+  <a href=""><img src="https://app.clockify.me/assets/nav-icons/dashboard.svg" alt=""><span>DASHBOARD</span></a>
+ 
+  <a href="report.html"><img src="https://app.clockify.me/assets/nav-icons/reports.svg" alt="" onclick="openreportpopup()"><span>REPORTS</span></a>
+ 
+  <a href=""><span>MANAGE</span></a>
+ 
+  <a href="project.html"><img src="https://app.clockify.me/assets/nav-icons/projects.svg" alt=""><span>PROJECTS</span></a>
+ 
+  <a href=""><img src="https://app.clockify.me/assets/nav-icons/teams.svg" alt=""><span>TEAM</span></a>
+
+  <a href=""><img src="https://app.clockify.me/assets/nav-icons/clients.svg" alt=""><span>CLIENT</span></a>
+
+  <a href=""><img src="https://app.clockify.me/assets/nav-icons/tags.svg" alt=""><span>TAGS</span></a>
+
+  <a href=""><img src="https://app.clockify.me/assets/nav-icons/settings.svg" alt=""><span>SETTING</span></a>
+
+</div>
+  `
+}
+
+export default sidebar1
---
+++ scripts/sidebar.js
@@ -0,0 +1,6 @@
+import  sidebar1  from "../components/sidebar1.js";
+
+let a=document.querySelector(".sidebar");
+a.innerHTML=sidebar1();
+
+console.log(sidebar1)
