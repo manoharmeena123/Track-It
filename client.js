a5c9d076c69e168a84d37cb1374a66e18cfed8cb
Ananyakumarisingh <ananyakumarisingh77@gmail.com>
2023-02-23 14:15:55 +0530

    day1

--- client.js
+++ client.js
@@ -1,3 +1,6 @@
+function editClient() {
+    document.querySelector(".bg-model").style.display = 'flex';
+};
 function close_details() {
     document.querySelector(".bg-model").style.display = 'none';
 };
@@ -26,28 +29,24 @@ function displayClient(array) {
         <div id="client_address">${item.address}</div>
         <div class="icons">
             <div id="note_icon"><span class="material-symbols-outlined">description</span></div>
-            <div class="edit_icon" onClick="edit()"><span class="material-symbols-outlined">edit</span></div>
+            <div class="edit_icon" onClick="editClient()"><span class="material-symbols-outlined">edit</span></div>
             <div id="show_more"><span class="material-symbols-outlined">more_vert</span></div>
         </div>
         </div>`
     }).join("");
     
-    // let editClient = document.querySelector(".material-symbols-outlined");
-    // console.log(editClient)
-    // for(let key of editClient){
-    //     key.addEventListener("click",(event)=>{
-    //     document.querySelector(".bg-model").style.display = 'flex';
-    //               let dataId = event.target.dataset.id;
-    //               console.log(dataId);
-    //     });
-    //   };
+    let editClient = document.querySelector(".material-symbols-outlined");
+    console.log(editClient)
+    for(let key in editClient){
+        // key.addEventListener("click",(event)=>{
+        document.querySelector(".bg-model").style.display = 'flex';
+                  let dataId = event.target.dataset.id;
+                  console.log(dataId);
+        };
+      };
     
 
-}
+
 let array = JSON.parse(localStorage.getItem("clientDetails"));
 displayClient(array);
 
-
-function editClient() {
-    document.querySelector(".bg-model").style.display = 'flex';
-};
