0a4d942d7d710bc58c72653a1306b7ed19aed475
Ananyakumarisingh <ananyakumarisingh77@gmail.com>
2023-02-24 21:47:37 +0530

    2nd popup page

--- team.js
+++ team.js
@@ -1,6 +1,12 @@
 function changeRate() {
     document.querySelector(".bg-model").style.display = 'flex';
 };
-function closeRate() {
+function changeRole() {
+    document.querySelector(".bg-model_role").style.display = 'flex';
+};
+function closeRole() {
     document.querySelector(".bg-model").style.display = 'none';
+};
+function closeRole() {
+    document.querySelector(".bg-model_role").style.display = 'none';
 };
