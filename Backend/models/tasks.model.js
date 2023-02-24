a0ad45540f790f5a59b14d198e8ec51b6c9bf9d1
Anandhu_P_A <anandhupa131@gmail.com>
2023-02-24 22:36:47 +0530

    test1

--- Backend/models/tasks.model.js
+++ Backend/models/tasks.model.js
@@ -1,1 +1,18 @@
-trying to add new FontFaceSetLoadEvent
+const mongoose = require('mongoose');
+const {ProjectModel}=require("./projects.model")
+const Schema = mongoose.Schema;
+const taskSchema  = mongoose.Schema({
+    task : {type:String,required:true},
+    date : {type:String,required:true},
+    startTime : {type:String,required:true} ,
+    endTime: {type:String, required:true},
+    userId : {type:Schema.Types.ObjectId,ref:"user"},
+    projectId:{type:Schema.Types.ObjectId,ref:"project"},
+    totalTimeInSec : {type:Number}
+
+},{versionKey:false})
+
+const TaskModel = mongoose.model("task",taskSchema);
+
+
+module.exports = {TaskModel};
