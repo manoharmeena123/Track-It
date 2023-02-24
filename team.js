3db80084bf0ae49616dc76140452f15821372b2d
Ananyakumarisingh <ananyakumarisingh77@gmail.com>
2023-02-24 20:31:32 +0530

    team page and 1st popup page

---
+++ team.js
@@ -0,0 +1,6 @@
+function changeRate() {
+    document.querySelector(".bg-model").style.display = 'flex';
+};
+function closeRate() {
+    document.querySelector(".bg-model").style.display = 'none';
+};
