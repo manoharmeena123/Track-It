01b46edfbba666b155d5c8ce08c070de9c6a535f
Ananya Kumari Singh <112763650+Ananyakumarisingh@users.noreply.github.com>
2023-02-25 10:44:09 +0530

    Ananya day2 (#17)
    * day2 first commit
    
    * projects page
    
    * linking the files client and projects
    
    * project popup page
    
    * team page and 1st popup page
    
    * 2nd popup page
    
    * teams_page commented appended div

---
+++ team.js
@@ -0,0 +1,12 @@
+function changeRate() {
+    document.querySelector(".bg-model").style.display = 'flex';
+};
+function changeRole() {
+    document.querySelector(".bg-model_role").style.display = 'flex';
+};
+function closeRate() {
+    document.querySelector(".bg-model").style.display = 'none';
+};
+function closeRole() {
+    document.querySelector(".bg-model_role").style.display = 'none';
+};
