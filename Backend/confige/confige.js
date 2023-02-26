1acb6a8eb501fd340613bfc39871c89ef28d8e78
Ejajul Ansari <112761880+Ejajkhan613@users.noreply.github.com>
2023-02-26 14:24:18 +0530

    Delete Backend directory

--- Backend/confige/confige.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-require("dotenv").config()
-mongoose.set('strictQuery',true)
-
-const connection = mongoose.connect(process.env.mongourl)
-
-module.exports={
-    connection
-}
