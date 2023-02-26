bbd48a97d66f4567b1f61491daefa54416f267c1
Manohar Meena <108083768+manoharmeena123@users.noreply.github.com>
2023-02-26 21:37:29 +0530

    Update team.model.js

--- Backend/models/team.model.js
+++ Backend/models/team.model.js
@@ -4,7 +4,7 @@ const teamSchema  = mongoose.Schema({
     name : {type:String,required:true},
     email : {type:String,required:true},
     billableRate : {type:Number,required:true} ,
-    role: {type:String, required:true},
+    role: {type:String, required:true,enum:["admin","user"] ,default:"admin"},
     userId : {type:Schema.Types.ObjectId,ref:"user"},
     
 
