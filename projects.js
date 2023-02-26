438cbc057c70448b579d17eaf892df0215940864
Ejajul Ansari <112761880+Ejajkhan613@users.noreply.github.com>
2023-02-26 14:27:30 +0530

    Delete projects.js

--- projects.js
+++
@@ -1,6 +0,0 @@
-function addProjects() {
-    document.querySelector(".bg-model").style.display = 'flex';
-};
-function closeProjects() {
-    document.querySelector(".bg-model").style.display = 'none';
-};
